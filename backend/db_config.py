82915a4b939408a9b3ae208aec2126434a11fe80
Vishalini-27 <vishalinip27@gmail.com>
2025-07-23 22:36:46 +0530

    Update db_config.py

--- backend/db_config.py
+++ backend/db_config.py
@@ -4,7 +4,7 @@ def get_connection():
     return mysql.connector.connect(
         host='bay9r1xqivp65pupciyt-mysql.services.clever-cloud.com',           
         user='uetsbhu157iqoriu',
-        password='4GWUtfRo8FFf1b8vQhFn',
+        password='your_pwd',
         database='bay9r1xqivp65pupciyt',
         port=3306              
     )
